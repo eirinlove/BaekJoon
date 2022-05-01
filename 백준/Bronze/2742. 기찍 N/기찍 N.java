e744a3075eb80b12b1fb9d61a0901cde5812faf7
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-02 00:08:16 +0900

    [Bronze III] Title: 기찍 N, Time: 1020 ms, Memory: 32912 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int i = scan.nextInt();
+		
+		
+		for (int j = i; j>=1; j-- ) {
+			
+			
+			System.out.println(j);
+			
+		}
+	}
+	
+	
+}
