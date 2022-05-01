79fe99511502fc84cf25544e23e441b0fd6cc058
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-01 23:16:57 +0900

    [Bronze III] Title: A+B - 3, Time: 220 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+
+public static void main(String[] args) {
+	
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int i = scan.nextInt();
+
+	
+	for ( int num=i; num>0; num-- ) {
+		
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		System.out.println(x+y);
+		
+	}
+	
+	
+	
+}
+	
+
+	
+}
