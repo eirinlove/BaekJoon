9406eb83fd4b70178ccc60e714b1180303ad0507
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-03 01:22:16 +0900

    [Bronze III] Title: A+B - 7, Time: 232 ms, Memory: 17852 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int x = scan.nextInt();
+		
+		
+		
+		for (int i=1; i<=x; i++) {
+			
+			
+			int a = scan.nextInt();
+			int b  = scan.nextInt();
+			
+			System.out.println("Case #"+i+": "+(a+b));
+		}
+		
+		scan.close();
+	}
+}
