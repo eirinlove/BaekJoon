7e264291cc5f9835e553bf3c8e74cab7aab48a65
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-06 22:01:55 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	
+	public static void main(String[] args ) {
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( \')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+}
