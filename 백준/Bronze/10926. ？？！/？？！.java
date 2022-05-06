8b2c24dd1aadd9e5a8751bc6a08939a84ebdf25f
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-06 21:59:57 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Main{
+    
+    
+ public static void main(String[] args){
+     
+     Scanner scan = new Scanner(System.in);
+     
+         String name = scan.next();
+         System.out.println(name+"??!");
+     
+ }
+    
+}
