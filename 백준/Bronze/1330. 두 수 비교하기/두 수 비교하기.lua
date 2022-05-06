eaa423776528823d2d36fe95d8e0ec63b1543189
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-06 22:05:52 +0900

    [Bronze IV] Title: 두 수 비교하기, Time: 0 ms, Memory: 9764 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.lua
@@ -0,0 +1,20 @@
+num1 = io.read("*n")
+num2 = io.read("*n")
+
+
+if num1 == num2 then
+    print("==")
+   end
+
+if num1 < num2 then
+    print("<")
+   end
+
+if num1 > num2 then
+    print(">")
+   end
+
+
+
+
+
