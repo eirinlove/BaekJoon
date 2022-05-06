940ab5b76dc420b84021455038a1e9d2aefe252c
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-06 22:03:45 +0900

    [Bronze IV] Title: 윤년, Time: 216 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) { 
+    	
+    	
+    	Scanner scan = new Scanner(System.in);
+    	
+    	
+    	int a = scan.nextInt();
+    	
+    	
+    	if ( a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
+    		System.out.println("1");
+    	}
+    	else {
+    		System.out.println("0");
+    	}
+}
+    
+
+}
