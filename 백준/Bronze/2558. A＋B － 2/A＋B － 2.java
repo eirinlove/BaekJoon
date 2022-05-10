aa519ff6da665fb139877b07fc8c61dd87b8e1a4
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-11 06:09:04 +0900

    [Bronze V] Title: A+B - 2, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		scan.close();
+		System.out.println(num1+num2);
+		
+		
+	}
+}
