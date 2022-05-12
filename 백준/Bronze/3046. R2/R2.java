1c43efecd287709108c9f44a9923e27d7216beda
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-13 04:05:16 +0900

    [Bronze V] Title: R2, Time: 204 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args) {
+	
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int i = scan.nextInt();
+	int j = scan.nextInt();
+	
+	System.out.println((j*2)-i);
+	
+	//S*2-R1
+}
+	
+	
+}
