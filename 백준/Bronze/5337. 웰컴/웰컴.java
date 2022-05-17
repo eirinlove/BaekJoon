c9de4b076dc1a1e10b1823a6482166bfc1ccc774
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-17 10:23:30 +0900

    [Bronze V] Title: 웰컴, Time: 108 ms, Memory: 13840 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+	
+}
