bae96594586a41c5267e1aba821fefdda3310655
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-05-28 15:57:57 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 252 ms, Memory: 17948 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	
+	Scanner scan = new Scanner(System.in);
+	
+	
+	while(scan.hasNext()) {
+	
+		String str = scan.nextLine();
+		System.out.println(str);
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
+}
