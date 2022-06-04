b43c8e7412be9481b8739d81b9bc8975182b29ed
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-06-04 23:42:03 +0900

    [Bronze V] Title: 한글 2, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		String han = scan.next();
+		
+		
+		char get = han.charAt(0);
+		System.out.println((int)get - 44031);
+		
+		
+	}
+	
+}
