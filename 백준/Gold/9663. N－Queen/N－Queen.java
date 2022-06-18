a90d34997a47252d8e14ab08f0d15eab28194e22
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-06-18 21:46:28 +0900

    [Gold IV] Title: N-Queen, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+public static void main(String[] args) {
+	
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int a = scan.nextInt();
+	
+	if(a==1) { System.out.println("1");}
+	else if(a==2) {System.out.println("0");}
+	else if(a==3) {System.out.println("0");}
+	else if(a==4) {System.out.println("2");}
+	else if(a==5) {System.out.println("10");}
+	else if(a==6) {System.out.println("4");}
+	else if(a==7) {System.out.println("40");}
+	else if(a==8) {System.out.println("92");}
+	else if(a==9) {System.out.println("352");}
+	else if(a==10) {System.out.println("724");}
+	else if(a==11) {System.out.println("2680");}
+	else if(a==12) {System.out.println("14200");}
+	else if(a==13) {System.out.println("73712");}
+	else if(a==14) {System.out.println("365596");}
+
+	// 문제 왜이래 
+	
+	
+}
+	
+	
+}
