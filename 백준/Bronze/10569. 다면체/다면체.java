1be89723f95b1c04e315e5a31b3b6ca56470e629
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-06-22 21:43:05 +0900

    [Bronze III] Title: 다면체, Time: 212 ms, Memory: 17448 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int a = scan.nextInt();
+	
+	while(a!=0) {
+		a--;
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		System.out.println((c-b)+2);
+		
+		
+	}
+}
+}
