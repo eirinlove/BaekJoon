d315ad2e97328597c4f3fd8aff4a91a6abaaf7c7
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-07-05 23:23:44 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	
+	
+	long a = scan.nextLong();
+	long b = scan.nextLong();
+	long c = scan.nextLong();
+	
+	System.out.println(a+b+c);
+}
+}
