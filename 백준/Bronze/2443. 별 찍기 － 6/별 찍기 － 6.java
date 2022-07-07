5e4d90d2210240acb95026c20303954c49b82ffe
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-07-07 22:32:41 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 396 ms, Memory: 18784 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int n = scan.nextInt();
+	
+	for(int i=0; i<n; i++) {
+		
+		
+
+		
+		for(int y=i; y<(n*2)-(i+1); y++) {
+			
+			System.out.print("*");
+			
+			
+		}
+		
+
+		System.out.println();
+		
+		
+		for(int j=0; j<i+1; j++) {
+			
+			System.out.print(" ");
+			
+		}
+		
+	}
+	
+	
+	
+}
+}
