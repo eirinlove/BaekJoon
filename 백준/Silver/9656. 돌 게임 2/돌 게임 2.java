b29d4ee3075a7bf1e0fa756198af38b229b340a3
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-07-18 22:06:40 +0900

    [Silver V] Title: 돌 게임 2, Time: 216 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		
+		
+		if(N % 2 == 1) {
+			
+			System.out.println("CY");
+			
+		}
+		
+		else {
+			
+			System.out.println("SK");
+		}
+		
+	}
+}
