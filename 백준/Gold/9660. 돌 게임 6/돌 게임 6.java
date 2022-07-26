913a6689aaf890ef7878d6977c209a7d97fdaffe
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-07-26 22:07:06 +0900

    [Gold V] Title: 돌 게임 6, Time: 212 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Gold/9660. 돌 게임 6/돌 게임 6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		long N = scan.nextLong();
+		
+		
+		if(N % 7 == 0 || N % 7 == 2 ) {
+			
+			System.out.println("CY");
+			
+		}
+		
+		else {
+			
+			System.out.println("SK");
+		}
+		
+	}
+}
