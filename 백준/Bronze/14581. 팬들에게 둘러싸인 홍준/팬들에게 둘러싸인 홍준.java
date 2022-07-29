9060a4efd7ef8718ce6d8617535443b6fc553664
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-07-29 23:06:09 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 216 ms, Memory: 17508 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	String j =scan.next();
+	
+	System.out.println(":fan:"+":fan:"+":fan:");
+	System.out.println(":fan:"+":"+j+":"+":fan:");
+	System.out.println(":fan:"+":fan:"+":fan:");
+	
+	
+}
+}
