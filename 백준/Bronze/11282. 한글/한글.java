6f8510463ebdbadd03a8c3f0fabfb0d3c599829f
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-08-06 22:30:35 +0900

    [Bronze IV] Title: 한글, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner scan= new Scanner(System.in);
+		
+		int i = scan.nextInt();
+		
+		i = i+44031;
+		char han = (char) i;
+		
+		System.out.println(han);
+	}
+}
