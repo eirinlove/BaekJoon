0df2f1e89772bb48604006b8f2472f98408eb28d
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-08-09 23:08:13 +0900

    [Bronze V] Title: 나는 행복합니다~, Time: 204 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		System.out.print((c/b)+" "+(c%b));
+		
+		
+		
+	}
+}
+
