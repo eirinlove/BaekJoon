7bf7c9fcfa21be563215c249f2277d5ac7acdec7
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-08-21 20:20:41 +0900

    [Bronze IV] Title: 과자, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		if((a*b)-c>=0){
+		System.out.println((a*b)-c);
+			
+		}
+		else {
+			
+			System.out.println("0");
+		}
+		
+	}
+}
