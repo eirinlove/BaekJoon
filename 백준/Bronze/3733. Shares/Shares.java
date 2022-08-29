227b2a90dedeaaf494a0d8a1f99c74bd67b3b1e7
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-08-29 22:19:03 +0900

    [Bronze V] Title: Shares, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+	Scanner scan = new Scanner(System.in);	
+	
+
+	
+	while(scan.hasNext()) {
+		
+		
+	int a = scan.nextInt();
+	int b = scan.nextInt();
+	System.out.println(b/(a+1));
+	
+	}
+	
+	
+	}
+}
