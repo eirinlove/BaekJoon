08172ce3bb940977d150e96b9fa81e75e407551a
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-09-15 20:04:13 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 136 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		
+		Date date = new Date();
+		
+	
+		System.out.println(date.getYear()+1900);
+		System.out.println(date.getMonth()+1);
+		System.out.println(date.getDate());
+		
+		
+	}
+}
