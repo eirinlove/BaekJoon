08b5169932feda4d1de64ee912c6da7402cf0b5e
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-09-17 21:30:48 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 204 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int a = scan.nextInt();
+	
+	System.out.print((a+1)*2+" "+(a+1)*3);
+	
+}
+}
