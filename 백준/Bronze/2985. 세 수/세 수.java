59f067f47343b7357a68601d1343fc80315d53d4
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-09-20 18:11:49 +0900

    [Bronze III] Title: 세 수, Time: 208 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+
+	
+	int a = scan.nextInt();
+	int b = scan.nextInt();
+	int c = scan.nextInt();
+	
+	if(a+b==c){System.out.println(a+"+"+b+"="+c);}
+	else if (a-b==c){System.out.println(a+"-"+b+"="+c);}
+	else if (a*b==c){System.out.println(a+"*"+b+"="+c);}
+	else if (a/b==c){System.out.println(a+"/"+b+"="+c);}
+	else if (a==b+c){System.out.println(a+"="+b+"+"+c);}
+	else if (a==b-c){System.out.println(a+"="+b+"-"+c);}
+	else if (a==b*c){System.out.println(a+"="+b+"*"+c);}
+	else if (a==b/c){System.out.println(a+"="+b+"/"+c);}
+	
+	
+}
+}
