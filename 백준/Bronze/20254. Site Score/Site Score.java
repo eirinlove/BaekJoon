961964097ace75385aaf6120b149c95b145d131c
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-09-26 17:40:49 +0900

    [Bronze V] Title: Site Score, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	int UR = scan.nextInt();
+	int TR = scan.nextInt();
+	int UO = scan.nextInt();
+	int TO = scan.nextInt();
+	System.out.println((56*UR)+(24*TR)+(14*UO)+(6*TO));
+	
+	
+}
+}
