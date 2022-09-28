cae8b05b3fe0315d873cedc89d41d5f2b1442703
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-09-28 20:35:03 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 204 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	int a = scan.nextInt();
+	a = (int) Math.pow(a,3);
+	System.out.println(a);
+	
+}
+}
