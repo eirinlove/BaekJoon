aeb8a79edc52ebb4b686a1a58f182643a8d8de12
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-10-02 22:19:10 +0900

    [Bronze IV] Title: 計算 (Calculation), Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/24075. 計算 （Calculation）/計算 （Calculation）.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	
+	int a = scan.nextInt();
+	int b = scan.nextInt();
+
+	if(a+b > a-b) {
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+	}
+	else {
+		System.out.println(a-b);
+		System.out.println(a+b);
+		
+	}
+	
+	
+	
+}
+}
