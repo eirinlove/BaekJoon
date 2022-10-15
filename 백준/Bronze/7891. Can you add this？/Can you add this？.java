1eb663caeedb1c8b9c450786ec49cf72a55f63e3
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-10-15 18:36:58 +0900

    [Bronze IV] Title: Can you add this?, Time: 224 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	int n = scan.nextInt();
+	
+	while(n!=0) {
+		
+	int a = scan.nextInt();
+	int b = scan.nextInt();
+	System.out.println(a+b);
+	n--;
+	}
+}
+}
