4243f63f35232bf8ed46658d4fbba9c1d4c68602
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-10-18 20:53:47 +0900

    [Bronze V] Title: SciComLove, Time: 208 ms, Memory: 17508 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	
+	Scanner scan = new Scanner(System.in);
+	int a = scan.nextInt();
+	
+	while(a!=0) {
+		
+		System.out.println("SciComLove");
+		a--;
+	}
+}
+}
