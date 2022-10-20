507c91b0e403444bcd8b8b505c7b724cd98b4ebd
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-10-20 20:47:14 +0900

    [Bronze IV] Title: CAPS, Time: 444 ms, Memory: 27856 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	String n = scan.next();
+	
+	System.out.println(n.toUpperCase());
+	
+	
+}
+}
