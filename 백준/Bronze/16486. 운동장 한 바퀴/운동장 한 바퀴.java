e19dc6ca7fc70359a768ac6f7cdb35ae50b4eb66
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-10-23 21:38:16 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 212 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int a = scan.nextInt();
+		double py = 3.141592;
+		System.out.println((n*2 + 2*py*a));
+		
+	}
+	
+}
