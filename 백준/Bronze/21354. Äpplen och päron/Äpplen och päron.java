41f4277b98bfe83003bed72d7921024c86d0639f
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-10-26 21:34:38 +0900

    [Bronze IV] Title: Äpplen och päron, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/21354. Äpplen och päron/Äpplen och päron.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		a= a*7;
+		b= b*13;
+		
+		if(a>b){
+			System.out.println("Axel");
+		}
+		else if (b>a){
+			System.out.println("Petra");
+		}
+		else{
+			
+			System.out.println("lika");
+		}
+	}
+}
