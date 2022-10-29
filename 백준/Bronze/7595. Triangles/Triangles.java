4908f98e969f243e0e9d7d41e81b32d7588ff59a
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-10-29 18:02:48 +0900

    [Bronze IV] Title: Triangles, Time: 1916 ms, Memory: 42920 KB -BaekjoonHub

---
+++ 백준/Bronze/7595. Triangles/Triangles.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	
+	Scanner  scan = new Scanner(System.in);
+	
+	while(true) {
+	int n = scan.nextInt();
+	
+	for(int i = 1; i<=n; i++) {
+		
+		for(int j = 1; j<=i; j++) {
+			
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+	
+	if(n==0) {
+		
+		break;
+	}
+	}
+}
+}
