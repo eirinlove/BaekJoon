191dfece6c49afc60786864bf508e0e473b65ffb
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-11-05 21:26:36 +0900

    [Bronze III] Title: 평균 중앙값 문제, Time: 200 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/5691. 평균 중앙값 문제/평균 중앙값 문제.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	
+	while(true) {
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+	
+		if(a==0 && b==0) {
+			
+			break;
+		}
+		System.out.println((2*a)-b);
+	}
+	
+}
+}
