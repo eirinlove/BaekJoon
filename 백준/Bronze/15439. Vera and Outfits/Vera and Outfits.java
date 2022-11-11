900caa9c86e57e0f2b2ac170fd4956fa5e589348
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-11-11 21:12:30 +0900

    [Bronze IV] Title: Vera and Outfits, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. Vera and Outfits/Vera and Outfits.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	int i = scan.nextInt();
+	
+	System.out.println((i*i)-i);
+}
+}
