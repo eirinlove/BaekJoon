bce2329b223229d65bf4ade6c5a5958b4ecc82bc
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-11-28 20:11:10 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 200 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	
+	
+	
+	while(scan.hasNextLine()) {
+	String a = scan.nextLine();
+
+	
+	System.out.println(a);
+	
+	}
+}
+}
