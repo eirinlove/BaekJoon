0e5cccab1991f9b54c13737ee1bfb10e0541f14b
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-12-25 23:34:43 +0900

    [Bronze V] Title: Serca, Time: 4100 ms, Memory: 71652 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	int a = scan.nextInt();
+	
+	while(a!=0) {
+		
+		System.out.println(" @@@   @@@ ");
+		System.out.println("@   @ @   @");
+		System.out.println("@    @    @");
+		System.out.println("@         @");
+		System.out.println(" @       @ ");
+		System.out.println("  @     @  ");
+		System.out.println("   @   @   ");
+		System.out.println("    @ @    ");
+		System.out.println("     @     ");
+		a--;
+	}
+}
+}
