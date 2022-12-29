a565fb88da54927453d45b15ad67cbe2f171902b
UTAZFOONE <48840424+eirinlove@users.noreply.github.com>
2022-12-29 23:01:32 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	int n = scan.nextInt();
+	System.out.println(n);
+	System.out.println(1);
+	
+}
+}
